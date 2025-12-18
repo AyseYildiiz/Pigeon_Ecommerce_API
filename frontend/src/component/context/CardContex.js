c851f596190be2ae9e22f81c857539fc12483585
Ayse Yildiz <ayseyildizz1923@gmail.com>
2025-12-18 21:11:41 +0300

    Guard and CardContex has added

---
+++ frontend/src/component/context/CardContex.js
@@ -0,0 +1,81 @@
+import { createContext, useEffect, useReducer } from "react";
+
+const CartContext = createContext();
+
+const initialState = {
+  cart: JSON.parse(localStorage.getItem("cart")) || [],
+};
+
+//Cart operations
+const cartReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_ITEM": {
+      //identify existing item
+      const existingItem = state.cart.find(
+        (item) => item.id === action.payload.id
+      );
+      let newCart;
+
+      if (existingItem) {
+        newCart = state.cart.map((item) =>
+          item.id === action.payload.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        );
+      } else {
+        newCart = [...state.cart, { ...action.payload, quantity: 1 }];
+      }
+
+      localStorage.setItem("cart", JSON.stringify(newCart));
+      return { ...state, cart: newCart };
+    }
+    case "REMOVE_ITEM": {
+      const newCart = state.cart.filter(
+        (item) => item.id !== action.payload.id
+      );
+      localStorage.setItem("cart", JSON.stringify(newCart));
+      return { ...state, cart: newCart };
+    }
+
+    case "INCREMENT_ITEM": {
+      const newCart = state.cart.map((item) =>
+        item.id === action.payload.id
+          ? { ...item, quantity: item.quantity + 1 }
+          : item
+      );
+      localStorage.setItem("cart", JSON.stringify(newCart));
+      return { ...state, cart: newCart };
+    }
+
+    case "DECREMENT_ITEM": {
+      const newCart = state.cart.map((item) =>
+        item.id === action.payload.id && item.quantity > 1
+          ? { ...item, quantity: item.quantity - 1 }
+          : item
+      );
+      localStorage.setItem("cart", JSON.stringify(newCart));
+      return { ...state, cart: newCart };
+    }
+
+    case "CLEAR_ITEM": {
+      localStorage.removeItem("cart");
+      return { ...state, cart: newCart };
+    }
+    default:
+      return state;
+  }
+};
+
+export const CardProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(cartReducer, initialState);
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(state.cart));
+  }, [state.cart]);
+  return (
+    <CartContext.Provider value={{ cart: state.cart, dispatch }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export const useCart = () => useContext(CartContext);
